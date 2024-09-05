b3451c9ff125de1ce30c83777df565ef4408e66e
Anand Verma <anandverma552@gmail.com>
2024-09-05 00:03:34 -0400

    Adding serializers

--- cados_api/base/models.py
+++ cados_api/base/models.py
@@ -15,4 +15,4 @@ class Advocate(models.Model):
     # updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return self.name
+        return self.username
--- cados_api/base/views.py
+++ cados_api/base/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Advocate
 
+from .serializers import AdvocateSerializer
 
 @api_view(['GET'])
 def endpoints(request):
@@ -11,8 +13,10 @@ def endpoints(request):
 
 @api_view(['GET'])
 def advocates(request):
-    data = ['Anand', 'Subhashini', 'Saravanan']
-    return Response(data) 
+    # data = ['Anand', 'Subhashini', 'Saravanan']
+    advocates = Advocate.objects.all()
+    serializer = AdvocateSerializer(advocates, many=True)
+    return Response(serializer.data) 
 
 @api_view(['GET'])
 def advocate_detail(request, username):
