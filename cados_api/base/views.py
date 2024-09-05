c28d5184b4164197eddcd26163c7eb251cb5c69a
Anand Verma <anandverma552@gmail.com>
2024-09-05 01:14:02 -0400

    Adding a post method to add an advocate

--- cados_api/base/views.py
+++ cados_api/base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.db.models import Q
 
 from .models import Advocate
 
@@ -11,14 +12,25 @@ def endpoints(request):
     data = ['/advocates', '/advocates/:username']
     return Response(data)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def advocates(request):
-    query = request.GET.get('query')
-    if query == None:
-        query = ''
-    advocates = Advocate.objects.filter(username__icontains=query)
-    serializer = AdvocateSerializer(advocates, many=True)
-    return Response(serializer.data) 
+
+    if request.method == 'GET':
+        query = request.GET.get('query')
+        if query == None:
+            query = ''
+        advocates = Advocate.objects.filter(Q(username__icontains=query) | Q(bio__icontains=query))
+        serializer = AdvocateSerializer(advocates, many=True)
+        return Response(serializer.data) 
+
+    if request.method == 'POST':
+        data = request.data
+        advocate = Advocate.objects.create(
+            username = data['username'],
+            bio = data['bio']
+        )
+        serializer = AdvocateSerializer(advocate, many=False)
+        return Response(serializer.data)
 
 @api_view(['GET'])
 def advocate_detail(request, username):
